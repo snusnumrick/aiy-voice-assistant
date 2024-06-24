1954c38efecfed74c83d496667d4d3c501687c20
Anton Treskunov <anton@navtrac.com>
2024-06-24 16:22:41 -0700

    synthesize_speech

--- src/speech.py
+++ src/speech.py
@@ -2,13 +2,32 @@ import logging
 import openai
 import os
 from aiy.leds import (Leds, Pattern, PrivacyLed, RgbLeds, Color)
-
+from aiy.voice.audio import AudioFormat, Recorder, BytesPlayer
+from openai import OpenAI
+import tempfile
 
 logger = logging.getLogger(__name__)
+openai = OpenAI()
+RUN_DIR = '/run/user/%d' % os.getuid()
+AUDIO_SAMPLE_RATE_HZ = 16000
+AUDIO_FORMAT = AudioFormat(sample_rate_hz=AUDIO_SAMPLE_RATE_HZ,
+                           num_channels=1,
+                           bytes_per_sample=2)
 
 
 def synthesize_speech(text):
     logger.info('Synthesizing speech for: %s', text)
+    with tempfile.NamedTemporaryFile(suffix='.wav', dir=RUN_DIR) as f, \
+            openai.audio.speech.with_streaming_response.create(
+                model="tts-1-hd",
+                voice="onyx",
+                input=text
+            ) as response, \
+            BytesPlayer() as player:
+        play = player.play(AUDIO_FORMAT)
+        for data in response.iter_bytes:
+            play(data)
+        play(None)
 
 
 def transcribe_speech(button, leds):
