b0a4fa914d20fe2fb94b63c7934a933fa3751cbb
Anton Treskunov <anton@navtrac.com>
2024-06-24 19:17:17 -0700

    debug

--- main.py
+++ main.py
@@ -24,8 +24,9 @@ def main():
             player = FilePlayer()
 
             period_ms = 10 * 1000
-            leds.pattern = Pattern(period_ms, 0.03, period_ms * 0.1, period_ms * 0.1)
-            leds.update(Leds.rgb_pattern(Color.GREEN))
+            leds.pattern = Pattern.breath(period_ms)
+            DARK_GREEN = (0x00, 0x80, 0x00)
+            leds.update(Leds.rgb_pattern(DARK_GREEN))
 
             while True:
                 text = transcribe_speech(button, leds)
