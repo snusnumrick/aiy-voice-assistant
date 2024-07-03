d90039f42d1fa60aea798be71aeb0664f0eaa412
Anton Treskunov <anton@navtrac.com>
2024-07-03 12:54:37 -0700

    adding puctuation to recognition

--- src/audio.py
+++ src/audio.py
@@ -57,9 +57,9 @@ class GoogleSpeechRecognition(SpeechRecognitionService):
         logger.debug("Transcribing audio stream (google)")
         streaming_config = speech.types.StreamingRecognitionConfig(
             config=speech.types.RecognitionConfig(encoding=speech.types.RecognitionConfig.AudioEncoding.LINEAR16,
-                sample_rate_hertz=config.get("sample_rate_hertz", 16000),
-                language_code=config.get("language_code", "ru-RU"), enable_automatic_punctuation=True),
-            interim_results=True)
+                                                  sample_rate_hertz=config.get("sample_rate_hertz", 16000),
+                                                  language_code=config.get("language_code", "ru-RU"),
+                                                  enable_automatic_punctuation=True), interim_results=True)
         requests = (speech.types.StreamingRecognizeRequest(audio_content=chunk) for chunk in audio_generator)
         responses = self.client.streaming_recognize(streaming_config, requests)
 
@@ -89,11 +89,11 @@ class YandexSpeechRecognition(SpeechRecognitionService):
         self.recognize_options = stt_pb2.StreamingOptions(recognition_model=stt_pb2.RecognitionModelOptions(
             audio_format=stt_pb2.AudioFormatOptions(
                 raw_audio=stt_pb2.RawAudio(audio_encoding=stt_pb2.RawAudio.LINEAR16_PCM,
-                    sample_rate_hertz=config.get("sample_rate_hertz", 16000), audio_channel_count=1)),
-            text_normalization=stt_pb2.TextNormalizationOptions(
+                                           sample_rate_hertz=config.get("sample_rate_hertz", 16000),
+                                           audio_channel_count=1)), text_normalization=stt_pb2.TextNormalizationOptions(
                 text_normalization=stt_pb2.TextNormalizationOptions.TEXT_NORMALIZATION_ENABLED,
                 profanity_filter=config.get("profanity_filter", False),
-                literature_text=config.get("literature_text", True),
+                literature_text=config.get("literature_text", True)),
             language_restriction=stt_pb2.LanguageRestrictionOptions(
                 restriction_type=stt_pb2.LanguageRestrictionOptions.WHITELIST,
                 language_code=[config.get("language_code", "ru-RU")]),
