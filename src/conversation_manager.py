f574e0d133a04043232a8767797064acff90bff0
Anton Treskunov <anton@navtrac.com>
2024-07-13 16:14:15 -0700

    test

--- src/conversation_manager.py
+++ src/conversation_manager.py
@@ -162,7 +162,7 @@ class ConversationManager:
     def get_system_prompt(self):
         from src.responce_player import emotions_prompt
 
-        return ""
+        return self.config.get('system_prompt', self.default_system_prompt)
 
         prompt = f"{get_current_datetime_english(self.timezone)} {self.location} "
         prompt += self.config.get('system_prompt', self.default_system_prompt)
