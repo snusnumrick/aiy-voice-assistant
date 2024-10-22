cb8e2beeba6c98d0d0025825407a422429782fe7
Anton Treskunov <anton@navtrac.com>
2024-10-21 19:44:21 -0700

    Refactor PATH setup to occur after volume setting
    Rearranged the order of operations in `run.sh` to set the audio volume before adding local bin directories to the PATH. This ensures a logical sequence and potentially avoids issues if volume setting scripts rely on PATH adjustments. Output the updated PATH for verification.

--- run.sh
+++ run.sh
@@ -7,10 +7,6 @@ echo "--- End Audio Controls ---"
 # get path to script (project root)
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
-# add local bin directories to PATH
-HOME_DIR="$(echo "$SCRIPT_DIR" | cut -d'/' -f1-3)"
-export PATH=$HOME_DIR/.pyenv/shims:$HOME_DIR/.pyenv/bin:$HOME_DIR/.local/bin:$PATH
-
 # Navigate to the project directory
 cd "${SCRIPT_DIR}"
 
@@ -19,6 +15,11 @@ git pull
 
 # set audio volume
 amixer sset 'Master' 40% || amixer sset 'Speaker' 55% || echo "Failed to set volume"
+
+# add local bin directories to PATH
+HOME_DIR="$(echo "$SCRIPT_DIR" | cut -d'/' -f1-3)"
+export PATH=$HOME_DIR/.pyenv/shims:$HOME_DIR/.pyenv/bin:$HOME_DIR/.local/bin:$PATH
+echo $PATH
 
 # Run the Python script
 poetry run python main.py
